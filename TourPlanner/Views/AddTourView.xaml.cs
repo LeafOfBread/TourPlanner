ccd9dbdbc825c7ec52d1d5ba0ac1d1627ab90374
Daniel Gebhart <daniel.gebhart@hotmail.com>
2025-03-16 03:10:00 +0100

    Refactor AddTourView to use MVVM pattern
    Implemented AddTourViewModel with INotifyPropertyChanged.
    Introduced properties for TourName, TourFrom, TourTo, and TourDescription with change notifications.
    Added SaveTourCommand and defined SaveTour method with a TODO.
    Modified OnPropertyChanged to properly notify property changes.

--- TourPlanner/Views/AddTourView.xaml.cs
+++ TourPlanner/Views/AddTourView.xaml.cs
@@ -3,26 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.ComponentModel;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls;
+using TourPlannerClasses.Models;
 
-namespace TourPlanner.Views
+namespace TourPlanner.ViewModels
 {
-    /// <summary>
-    /// Interaction logic for AddTourView.xaml
-    /// </summary>
-    public partial class AddTourView : UserControl
+    public class AddTourViewModel : INotifyPropertyChanged
     {
-        public AddTourView()
+        private string _tourName;
+        private string _tourFrom;
+        private string _tourTo;
+        private string _tourDescription;
+
+        public string TourName
         {
-            InitializeComponent();
+            get => _tourName;
+            set
+            {
+                _tourName = value;
+                OnPropertyChanged(nameof(TourName));
+            }
+        }
+
+        public string TourFrom
+        {
+            get => _tourFrom;
+            set
+            {
+                _tourFrom = value;
+                OnPropertyChanged(nameof(TourFrom));
+            }
+        }
+
+        public string TourTo
+        {
+            get => _tourTo;
+            set
+            {
+                TourTo = value;
+                OnPropertyChanged(nameof(TourTo));
+            }
+        }
+
+        public string TourDescription
+        {
+            get => _tourDescription;
+            set
+            {
+                TourDescription = value;
+                OnPropertyChanged(nameof(TourDescription));
+            }
+        }
+
+        public ICommand SaveTourCommand { get; }
+
+        public AddTourViewModel()
+        {
+            SaveTourCommand = new RelayCommand(SaveTour);
+        }
+
+        public void SaveTour()
+        {
+            //TODO
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
