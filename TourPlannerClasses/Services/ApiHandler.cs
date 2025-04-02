21a5bc63a85a2f60f7757d1368cf4d6e4cb01e7f
Daniel Gebhart <daniel.gebhart@hotmail.com>
2025-04-03 01:18:47 +0200

    Add ApiHandler class for OpenRouteService integration
    Introduces the IApiHandler interface and the ApiHandler class, which implements the interface to fetch route information from the OpenRouteService API. The GetRouteInformation method is defined to make an asynchronous HTTP GET request using specified start and end coordinates. The method constructs the request URL with a hardcoded API key and currently returns null, indicating that the implementation is still in progress.

---
+++ TourPlannerClasses/Services/ApiHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlannerClasses.Models;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace TourPlanner.BusinessLogic.Services
+{
+    public interface IApiHandler
+    {
+        public Task<Tours> GetRouteInformation(string start, string end);
+    }
+
+    public class ApiHandler : IApiHandler           //unfinished, no idea if this actually works, took this code from the openrouteservices documentation. api key should work in theory
+    {
+        public string Url;
+        private readonly string ApiKey = "5b3ce3597851110001cf62485cf43c3fbd16446584cc2d3271c39cd0";
+
+        public async Task<Tours> GetRouteInformation(string start, string end)
+        {
+            var baseAddress = new Uri($"https://api.openrouteservice.org/v2/directions/driving-car?api_key={ApiKey}&start={start}&end={end}");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8");
+
+                using (var response = await httpClient.GetAsync("directions"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject(responseData);
+                }
+            }
+            return null;
+        }
+    }
+}
